7fab8636f733f1a79f792b3dab8f19e195ce2bcb
muhon9 <sultan.al.muhon@gmail.com>
2022-05-04 05:43:48 +0600

    Added token generator function

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -16,4 +16,15 @@ utilities.parseJSON = (str) => {
 // eslint-disable-next-line prettier/prettier
 utilities.hash = (str) => crypto.createHmac('sha256', environment.secreteKey).update(str).digest('hex');
 
+utilities.generateToken = (strlength) => {
+    const charOptions = 'abcdefghijklmnopqrstuvwxyz123456789';
+    let output = '';
+
+    for (let i = 0; i < strlength; i += 1) {
+        const randomChar = charOptions.charAt(Math.random() * charOptions.length);
+        output += randomChar;
+    }
+    return output;
+};
+
 module.exports = utilities;
